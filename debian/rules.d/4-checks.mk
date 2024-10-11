8adb6ccde3615be4787b1583d4cb231d123d0fd1
Pengcheng Xu <pengxu@ethz.ch>
2024-10-11 10:16:01 +0200

    Forked from Ubuntu-5.4.0-200.220 in focal
    Get full history from upstream focal tree at
    https://git.launchpad.net/~ubuntu-kernel/ubuntu/+source/linux/+git/focal/ ;
    Instructions at https://git-scm.com/book/en/v2/Git-Tools-Replace

---
+++ debian/rules.d/4-checks.mk
@@ -0,0 +1,14 @@
+checks-%: install-%
+	@echo Debug: $@
+
+# Check the config against the known options list.
+config-prepare-check-%: $(stampdir)/stamp-prepare-tree-%
+	@echo Debug: $@
+	if [ -e $(commonconfdir)/config.common.ubuntu ]; then \
+		perl -f $(DROOT)/scripts/config-check \
+			$(builddir)/build-$*/.config "$(arch)" "$*" "$(commonconfdir)" \
+			"$(skipconfig)" "$(do_enforce_all)"; \
+	else \
+		python3 $(DROOT)/scripts/misc/annotations -f $(commonconfdir)/annotations \
+			--arch $(arch) --flavour $* --check $(builddir)/build-$*/.config; \
+	fi
